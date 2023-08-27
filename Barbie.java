14ff67a75b418715e586f72480ee270a26a95808
ChirazBH <chiraz.benhasine@gmail.com>
2023-08-28 00:42:08 +0200

    first commit

---
+++ Barbie.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Décrivez votre classe Barbie ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Barbie
+{
+    private String nom;
+    private List<Robe> collection;
+
+    public Barbie(String nom) {
+        this.nom = nom;
+        this.collection = new ArrayList<>();
+    }
+
+    public void ajouterRobe(Robe robe) {
+        collection.add(robe);
+    }
+
+    public void afficherCollection() {
+        System.out.println("Collection de robes de " + nom + ":");
+        for (Robe robe : collection) {
+            System.out.println("- " + robe.getNom());
+        }}
+    }
---
+++ Robe.java
@@ -0,0 +1,19 @@
+
+/**
+ * Décrivez votre classe Robe ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Robe
+{
+       private String nom;
+
+    public Robe(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
